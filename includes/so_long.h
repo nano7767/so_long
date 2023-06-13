63ba7b87026fa4fbf141ec8f39e92162f7f829ca
nano7767 <126381073+nano7767@users.noreply.github.com>
2023-06-13 17:09:49 +0700

    added WALL_IMG path

--- includes/so_long.h
+++ includes/so_long.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   so_long.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: svikornv <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: svikornv <svikornv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/05 11:07:24 by svikornv          #+#    #+#             */
-/*   Updated: 2023/06/05 13:06:35 by svikornv         ###   ########.fr       */
+/*   Updated: 2023/06/13 16:55:52 by svikornv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,39 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <mlx.h>
 #include "libft.h"
+# define MAP_FILE	"maps/classic.ber"
+# define WALL_IMG	"maps/wall.xpm"
+typedef struct s_map
+{
+	int	width;
+	int	height;
+	char	**grid;
+	int	exit_count;
+	int	collectible_count;
+	int	player_count;
+}		t_map;
+
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int	bits_per_pixel;
+	int	line_length;
+	int	endian;
+}		t_data;
+
+typedef struct s_vars
+{
+	void	*mlx;
+	void	*win;
+}		t_vars;
+
+char	*extract_file(char *filename, t_map *map);
+char	**create_grid(t_map *map);
+void	fill_grid(char **grid, char *lines, t_map *map);
+void	draw_map(t_map *map);
 
 #endif
